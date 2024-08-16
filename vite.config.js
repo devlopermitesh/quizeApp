e3419e4a683fc80ded7b393a07059f9f40fd5423
unknown <gehlotmitesh63@gmail.com>
2024-08-16 11:43:55 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/quizeApp/', // Set this to your GitHub repository name
+});
